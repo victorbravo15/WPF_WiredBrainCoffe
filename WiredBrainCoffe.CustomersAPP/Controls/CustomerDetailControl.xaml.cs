7974c345fdab56e53ac4b1e00f61847227f4f3a3
Víctor Bravo Sánchez <sextoarecasvictor@gmail.com>
2023-09-27 14:59:03 +0200

    Optimización de código con Binding

--- WiredBrainCoffe.CustomersAPP/Controls/CustomerDetailControl.xaml.cs
+++ WiredBrainCoffe.CustomersAPP/Controls/CustomerDetailControl.xaml.cs
@@ -10,11 +10,9 @@ namespace WiredBrainCoffe.CustomersAPP.Controls
     /// </summary>
     public partial class CustomerDetailControl : UserControl
     {
-        private static bool isChangingSelectedCustomer;
-
         // Using a DependencyProperty as the backing store for Customer.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CustomerProperty =
-            DependencyProperty.Register("Customer", typeof(Customer), typeof(CustomerDetailControl), new PropertyMetadata(null, CustomerChangeCallback));
+            DependencyProperty.Register("Customer", typeof(Customer), typeof(CustomerDetailControl), new PropertyMetadata(null));
 
 
         public CustomerDetailControl()
@@ -26,40 +24,6 @@ namespace WiredBrainCoffe.CustomersAPP.Controls
         {
             get { return (Customer)GetValue(CustomerProperty); }
             set { SetValue(CustomerProperty, value); }
-        }
-
-
-        private static void CustomerChangeCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (d is CustomerDetailControl customerDetailControl)
-            {
-                var customer = e.NewValue as Customer;
-                isChangingSelectedCustomer = true;
-                customerDetailControl.txtFirstName.Text = customer?.FirstName ?? "";
-                customerDetailControl.txtLastName.Text = customer?.LastName ?? "";
-                customerDetailControl.chkIsDeveloper.IsChecked = customer?.IsDeveloper;
-                isChangingSelectedCustomer = false;
-            }
-        }
-
-        private void TextBox_IsChanged(object sender, TextChangedEventArgs e)
-        {
-            this.UpdateCustomer();
-        }
-
-        private void CheckBox_IsChanged(object sender, RoutedEventArgs e)
-        {
-            this.UpdateCustomer();
-        }
-
-        private void UpdateCustomer()
-        {
-            if (this.Customer != null && !isChangingSelectedCustomer)
-            {
-                this.Customer.FirstName = this.txtFirstName.Text;
-                this.Customer.LastName = this.txtLastName.Text;
-                this.Customer.IsDeveloper = this.chkIsDeveloper.IsChecked.GetValueOrDefault();
-            }
         }
     }
 }
